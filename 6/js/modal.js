0730713379d340496ff004af25d814ceeee27b30
Keks <keks@htmlacademy.ru>
2023-02-09 23:29:50 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/modal.js
@@ -0,0 +1,24 @@
+const searchHotel = document.querySelectorAll(".search-hotel");
+const searchHotelModal = document.querySelector(".modal-container");
+const modalClose = searchHotelModal.querySelector(".modal-close-button");
+
+for (let i = 0; i < searchHotel.length; i++) {
+  searchHotel[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    searchHotelModal.classList.add("modal-show");
+  });
+}
+
+modalClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  searchHotelModal.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (searchHotelModal.classList.contains("modal-show")) {
+      evt.preventDefault();
+      searchHotelModal.classList.remove("modal-show");
+    }
+  }
+});
